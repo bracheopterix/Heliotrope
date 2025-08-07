c513270925bbc4a9fd6477950c53d7877ed9485b
bracheopterix <bracheopterix@gmail.com>
2025-08-07 18:35:07 +0300

    stable 1 test

---
+++ README.md
@@ -0,0 +1,16 @@
+# Node + TypeScript + Express + Jest
+
+## *Is it a server?*
+- still doesn't use express.router, all routes in index.
+
+---------------
+# Heading 1
+## Heading 2
+**Bold text**
+*Italic text*
+- Bullet list
+1. Numbered list
+`inline code`
+---------------
+
+
--- index.ts
+++ index.ts
@@ -2,21 +2,53 @@ import express from "express";
 import cors from 'cors';
 import dotenv from 'dotenv';
 
-// this is your requests routes
-import users from './routes/users';
 
-// this is loading config from .env
-dotenv.config();
+const app = express(); // starting out serv
 
-// starting out serv
-const app = express();
-
-app.use(cors()); // launches cors ?
+dotenv.config(); // this is loading environmental config from .env
+app.use(cors()); // launches cors who knows what where and who in the internet lol
 app.use(express.json()); // a piece of middleware in Express that allows your server to parse incoming requests with JSON payloads
 
-// allows to correlate routers and routes??
-app.use('/api/users',users) 
+/// MAIN BLOCK /// 
 
-app.listen(process.env.PORT,()=>{
-    console.log(`Server is running on `)
+
+app.listen(process.env.PORT, () => { // I am listening on env.PORT port all of incoming data and if all running smoothly - telling about it
+    console.log(`Server is running on the port ${process.env.PORT}`,)
 })
+
+
+/// SIMPLE ROUTES ///
+
+app.get('/', (req, res) => {
+
+    try {
+        res.send("Helios vivit");
+    } catch (error) {
+        res.status(500).json({ error: "Failed to get an initial response" });
+    }
+
+})
+
+
+app.post('/', (req, res) => {
+
+    try {
+        res.send("Editing right is denied");
+    } catch (error) {
+        res.status(500).json({ error: "Failed to get a standart post response" });
+    }
+
+})
+
+
+
+app.get('/api/notes', (req, res) => {
+
+    try {
+        res.send(["note1", "note2", "note3"]);
+    } catch (error) {
+        res.status(500).json({ error: "Failed to get test note object" });
+    }
+
+})
+
